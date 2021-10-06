2e630070d42743ebaf7258670bcfafce8811ba73
anurag <anuragak60@gmail.com>
2021-10-06 12:26:11 +0530

    postgres+sqlalchemy basics added

--- falcon/app.py
+++ falcon/app.py
@@ -1,5 +1,5 @@
 import json, falcon
-from sqlalchemy import create_engine, and_, or_, not_, update, delete
+from sqlalchemy import create_engine, and_, or_, not_, update, delete, desc, func, cast
 import hashlib
 import re
 import jwt
@@ -10,6 +10,7 @@ from sqlalchemy.orm import sessionmaker, Session
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session, relationship
+from datetime import date
 
 
 engine = create_engine('postgresql://postgres:postgres@localhost:5432/socialmedia')
@@ -21,24 +22,83 @@ session = Session.configure(bind=engine)
 User = Base.classes.users   #Created tables
 Post = Base.classes.post
 Likes = Base.classes.likes
-
+Date = Base.classes.date
 
 s = Session()
 
 
-# def validate_positive_numbers(func):
-#     def wrapper(*args):
-#         for value in args:
-#             if value < 0:
-#                 raise ValueError('arguments must be greater than equal zero')
-#         return func(*args)
-#     return wrapper
+# join tables
+def single_join():
+    result = s.query(User, Post).join(Post).filter(Post.status=='Published').all()
+    for user, post in result:
+        return user.username, post.title
+        
+    result = s.query(Post, Likes).join(Likes).all()
+    for post, likes in result:
+        return post.title, 'liked user : ', likes.user_id
 
-# @validate_positive_numbers
-# def add(a, b):
-#     return a + b
-# print(add(7,5))
+def multiple_join():
+    result = s.query(User, Post, Likes).join(Post, Post.user_id==User.id).join(Likes, Likes.user_id==User.id).filter(Post.status=='Published').order_by(desc(Post.likes)).all()
+    for user, post, likes in result:
+        print('username: ', user.name)
+        print('published post: ', post.title)
+        print('liked post: ', likes.post_id)
+        print('*****************')
+    
 
+# min, max, sum
+def max():
+    result = s.query(func.max(Post.likes)).first()
+    return result
+max()
+
+def min():
+    result = s.query(func.min(Post.likes)).first()
+    return result
+min()
+
+def sum():
+    result = s.query(func.sum(Post.likes)).filter(Post.user_id==1).first()
+    return result
+sum()
+
+
+# and_ or_
+def and__():
+    result = s.query(Post).filter(and_(Post.likes==0, Post.status=='Published')).all()
+    for row in result:
+        print(row.title, row.user_id) 
+and__()
+
+def or__():
+    result = s.query(Post).filter(or_(Post.likes==0, Post.status=='Unpublished')).order_by(desc(Post.id)).all()
+    for row in result:
+        print(row.title)
+or__()
+
+# count
+def count_():
+    result = s.query(Post.status, func.count(Post.status)).filter(Post.status=='Published').group_by(Post.status).all()
+    return result
+count_()
+
+# average
+def avg_():
+    result = s.query(func.avg(User.ph_number)).first()
+    return result
+avg_()
+
+print(date.today())
+todate = date.today()
+
+def getdate():
+    result = s.query(Date).filter(func.date(Date.date)==todate).all()
+    for row in result:
+        print(row.date)
+getdate()
+
+# result = func.is_equal('q', 'hj')
+# print(result)
 
 class RegisterClass:                           # User registering with details
     
